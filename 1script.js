be4f764f3242d6c483ecd58bb645370ac2cfdbe7
Nicolas <93992407+Cloesick@users.noreply.github.com>
2022-01-27 08:15:59 +0100

    created weather forecast multiple days files + added css

---
+++ 1script.js
@@ -0,0 +1,3 @@
+// https://api.openweathermap.org/geo/1.0/direct?q=Brussels,Belgium&lang=en&appid=687a0170dedccd305846958f004f8301
+// apiKey = 687a0170dedccd305846958f004f8301
+
--- script.js
+++ script.js
@@ -25,12 +25,12 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         // console.log( name, icon, description, temp, humidity, speed)
-        document.querySelector(".city").innerText = "Weather in" + name;
+        document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src="https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C" ;
-        document.querySelector(".humidity").innerText = "Humidity" + "%";
-        document.querySelector(".wind").innerText = "Wind Speed" + speed + "km/h";
+        document.querySelector(".humidity").innerText = "Humidity " + "%";
+        document.querySelector(".wind").innerText = "Wind Speed " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + " ')";
     },
