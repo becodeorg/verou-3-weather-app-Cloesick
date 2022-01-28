aa8e31c53df1382d18fbf25dd8d86abfa4db8480
Cloesick <nicolas.cloet@hotmail.be>
2022-01-28 14:33:41 +0100

    final touches - still to include searchbar

--- final forecast/final.js
+++ final forecast/final.js
@@ -18,18 +18,18 @@ setInterval(() => {
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13? hour %12: hour;
+    const hoursIn12HrFormat = hour >= 13? hour %12: hour
     const minutes = time.getMinutes();
-    const ampm = hour >=12 ? 'PM' : 'AM';
+    const ampm = hour >=12 ? 'PM' : 'AM'
 
-    timeEl.innerHTML =
-			hoursIn12HrFormat +
-			":" +
-			minutes +
-			" " +
+    timeEl.innerHTML = 
+			(hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) +
+			':' +
+			(minutes <10? '0'+minutes: minutes) +
+			' ' + 
 			`<span id="am-pm">${ampm}</span>`;
 
-    dateEl.innerHTML = days[day] + ',' + date + '' +months[month]
+    dateEl.innerHTML = days[day] + ', ' + date + ' ' +months[month]
 
 }, 1000);
 
@@ -38,28 +38,23 @@ setInterval(() => {
 getWeatherData()
 function getWeatherData (){
     navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
-
-        
-
         let {latitude, longitude} = success.coords;
-
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric& exclude=hourly,minutely&appid=${API_KEY}`)
 					.then(res => res.json())
 					.then(data => {
-						console.log(data);
+						console.log(data)
                         showWeatherData(data);
-					});
-
+					})
     })
 }
     //console.log (getWeatherData)
-    function showWeatherData(data){
-        let {humidity, pressure, sunrise, sunset, wind_speed}
-    = data.current;
+function showWeatherData(data){
+        let {humidity, pressure, sunrise, sunset, wind_speed}= data.current;
+        timezone.innerHTML = data.timezone;
+        countryEl.innerHTML = 'North'+data.lat + 'East' + data.lon
 
-    currentWeatherItemsEl.innerHTML = `<div class="weather-item">
-        <div>Humidity</div>
+    currentWeatherItemsEl.innerHTML = 
+    `<div class="weather-item">
         <div>Humidity</div>
         <div>${humidity}%</div>
     </div>
@@ -86,22 +81,27 @@ function getWeatherData (){
     let otherDayForecast= ''
     data.daily.forEach((day, idx) => {
         if(idx == 0){
-            
+            currentTempEl.innerHTML = `
+            <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+            <div class="other">
+                <div class="day">${window.moment(day.dt*1000).format("dddd")}</div>
+                <div class="temp">Night - ${day.temp.night}&#176;C</div>
+                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+            </div>     
+            `
         }else{
             otherDayForecast += `
-<div class="weather-forecast-item">
-<div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
-<img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-<div class="temp">Night - ${day.temp.night}&#176; C</div>
-<div class="temp">Day - ${day.temp.day}&#176; C</div>
-</div>
-
-`;
+            <div class="weather-forecast-item">
+                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                <div class="temp">Day - ${day.temp.day}&#176; C</div>
+            </div>
+            `
         }
     })
 
     weatherForecastEl.innerHTML = otherDayForecast;
-
 }
 
 
